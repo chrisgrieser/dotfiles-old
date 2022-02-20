edaf8eec74a47ee7cb0da28ae7a0daf13b6f20ad
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-20 15:00:04 +0100

    Sun, 15:00, 2 files, M1 iMac
     M "Alfred.alfredpreferences/workflows/user.workflow.41B5E112-40CE-48A2-A145-9C26AAA57D85/Google Searches.log"
     M zsh/history_utils_config.zsh

--- zsh/history_utils_config.zsh
+++ zsh/history_utils_config.zsh
@@ -3,7 +3,6 @@
 export HISTSIZE=3000
 export SAVEHIST=3000
 export HISTFILE="$ZSH_DOTFILE_LOCATION"/.zsh_history
-export HIST_DATE_FORMAT='%A, %d.%m.%Y  %H:%M' # custom, defined by me
 
 setopt HIST_EXPIRE_DUPS_FIRST
 setopt HIST_IGNORE_DUPS
@@ -16,22 +15,23 @@ setopt EXTENDED_HISTORY
 
 # --------------------------
 
+export HIST_DATE_FORMAT='%a, %d.%m %H:%M   ' # custom, defined by me
+
 # SEARCH HISTORY FOR A COMMAND
 # enter ➞ write to buffer
 # alt+enter ➞ copy to clipboard
+
 DATE_CHAR_COUNT=$(echo "$HIST_DATE_FORMAT" | wc -m | tr -d " ")
-CUT_1=$((DATE_CHAR_COUNT + 15))
-CUT_2=$((DATE_CHAR_COUNT + 12))
+TO_CUT=$((DATE_CHAR_COUNT + 15))
 function hs {
-	SELECTED=$(history -t "$HIST_DATE_FORMAT" 1 | cut -c"$CUT_1"- | fzf \
+	SELECTED=$(history -t "$HIST_DATE_FORMAT" 1 | cut -c8- | sed -e 's/^/[1;33m/' | fzf \
 	           --tac --no-sort \
 	           --layout=reverse \
 	           --info=hidden \
 	           --query "$*" \
 	           --height=50% \
 	           --bind 'alt-enter:execute(echo {} | pbcopy)+abort' \
-	           --preview "grep {} \"$HISTFILE\" | cut -c3-13" \
-	           --preview-window=right:40% \
 	          )
-	print -z "$SELECTED"
+	COMMAND=$(echo "$SELECTED" | cut -c"$TO_CUT"-)
+	print -z "$COMMAND"
 }
