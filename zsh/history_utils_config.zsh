c491e23a9aa75468173d39b810ccb4feb28f56a4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-20 14:00:05 +0100

    Sun, 14:00, 2 files, M1 iMac
     M "Alfred.alfredpreferences/workflows/user.workflow.41B5E112-40CE-48A2-A145-9C26AAA57D85/Google Searches.log"
     M zsh/history_utils_config.zsh

--- zsh/history_utils_config.zsh
+++ zsh/history_utils_config.zsh
@@ -2,6 +2,8 @@
 
 export HISTSIZE=3000
 export SAVEHIST=3000
+export HISTFILE="$ZSH_DOTFILE_LOCATION"/.zsh_history
+
 setopt HIST_EXPIRE_DUPS_FIRST
 setopt HIST_IGNORE_DUPS
 setopt HIST_FIND_NO_DUPS
@@ -9,12 +11,13 @@ setopt HIST_REDUCE_BLANKS
 setopt APPEND_HISTORY
 setopt INC_APPEND_HISTORY
 setopt HIST_VERIFY
+setopt EXTENDED_HISTORY
 
 # search history for a command
 # enter ➞ write to buffer
 # alt+enter ➞ copy to clipboard
 function hs {
-	SELECTED=$(history 1 | cut -c8- | uniq | fzf \
+	SELECTED=$(history -E 1 | cut -c8- | uniq | fzf \
 	           --tac --no-sort \
 	           --layout=reverse \
 	           --info=hidden \
