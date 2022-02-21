f7e205199449ee3e5b90cff9396e3a8ce0e0ab2b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-21 16:30:05 +0100

    Mon, 16:30, 2 files, M1 iMac
     M "Alfred.alfredpreferences/workflows/user.workflow.41B5E112-40CE-48A2-A145-9C26AAA57D85/Google Searches.log"
     M zsh/docs_man.zsh

--- zsh/docs_man.zsh
+++ zsh/docs_man.zsh
@@ -13,12 +13,16 @@ autoload run-help
 alias help='run-help'
 export HELPDIR=/usr/share/zsh/5.8/help
 
+# use moar instead of less
+export PAGER="$(brew --prefix)"/bin/moar
+export MOAR="--style=dracula --no-linenumbers"
+
 # colorize less https://wiki.archlinux.org/index.php/Color_output_in_console#less .
-export LESS_TERMCAP_mb=$'\E[1;31m'     # begin bold
-export LESS_TERMCAP_md=$'\E[1;36m'     # begin blink
+# export LESS_TERMCAP_mb=$'\E[1;31m'     # begin bold
+export LESS_TERMCAP_md=$'\E[1;33m'     # begin blink
 export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
-export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
-export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+# export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+# export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
 export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
 export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
-export LESS='--status-column --tabs=3 --window=-4 --quit-at-eof'
+# export LESS='--status-column --tabs=3 --window=-4 --quit-at-eof'
