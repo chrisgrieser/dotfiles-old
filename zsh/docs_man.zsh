4c2d5d79ab6a35d67c486bd64791fd436527c1ad
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-21 17:30:06 +0100

    Mon, 17:30, 2 files, M1 iMac
     M "Alfred.alfredpreferences/workflows/user.workflow.41B5E112-40CE-48A2-A145-9C26AAA57D85/Google Searches.log"
     M zsh/docs_man.zsh

--- zsh/docs_man.zsh
+++ zsh/docs_man.zsh
@@ -19,10 +19,8 @@ export MOAR="--no-linenumbers"
 
 # colorize less https://wiki.archlinux.org/index.php/Color_output_in_console#less .
 export LESS_TERMCAP_mb=$'\E[1;31m'     # begin bold
-export LESS_TERMCAP_md=$'\E[1;35m'     # begin blink
+export LESS_TERMCAP_md=$'\E[1;33m'     # begin blink
 export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
-export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
-export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
-export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+export LESS_TERMCAP_us=$'\E[1;34m'     # begin underline
 export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
 export LESS='--status-column --tabs=3 --window=-5 --quit-at-eof'
