af0b1839ab0147750aacef432eb39de4b9c2626c
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-22 11:30:05 +0100

    Tue, 11:30, 3 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/title-case.js
     M "Sublime User Folder/bh_core.sublime-settings"

--- Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/title-case.js
+++ Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/title-case.js
@@ -1,32 +1,27 @@
 #!/usr/bin/env osascript -l JavaScript
 
-function run (argv){
-	var input = argv.join("");
+function run (argv) {
+	const input = argv.join("");
 
 	String.prototype.toTitleCase = function () {
-		const smallWords = new RegExp(/(?:an?d?|a[st]|because|but|by|en|for|i[fn]|neither|nor|o[fnr]|only|over|per|so|some|tha[tn]|the|to|up|upon|vs?\.?|versus|via|when|with|without|yet)/, 'i');
-	   let capitalized = this.replace(/\w\S*/g, function(word){
-			if (word.match(smallWords) != null){
-				return word.toLowerCase();
-			} else if (word == "i" || word == "I"){
-				return "I";
-			} else if (word.length < 3){
-				return word.toLowerCase();
-			}  else {
-				return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
-			}
-	   });
+		const smallWords = /(?:an?d?|a[st]|because|but|by|en|for|i[fn]|neither|nor|o[fnr]|only|over|per|so|some|tha[tn]|the|to|up|upon|vs?\.?|versus|via|when|with|without|yet)/i;
+		let capitalized = this.replace(/\w\S*/g, function(word) {
+			if (smallWords.test(word)) return word.toLowerCase();
+			if (word.toLowerCase() === "i") return "I";
+			if (word.length < 3) return word.toLowerCase();
+			return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+		});
 
-	   // Capitalize the beginning of a sentence,
-	   // or the first letter after a markdown heading
-	   capitalized = capitalized.replace (/[^#]*?(\n|\.|$)/g, function(sentence){
-	   	if (sentence.charAt(0) == " "){
-				return " " + sentence.charAt(1).toUpperCase() + sentence.substr(2);
-	   	} else {
-				return sentence.charAt(0).toUpperCase() + sentence.substr(1);
-	   	}
-	   });
-	   return capitalized;
+		// Capitalize the beginning of a sentence,
+		// or the first letter after a markdown heading
+		capitalized = capitalized.replace (/[^#]*?(\n|\.|$)/g, function(sentence) {
+			if (sentence.charAt(0) === " ")
+				return " " + sentence.charAt(1).toUpperCase() + sentence.slice(2);
+
+			return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+
+		});
+		return capitalized;
 	};
 
 	return input.toTitleCase();
