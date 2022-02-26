2936f5f12f03b06f25f6b2c029aaefb8cf2bac90
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-26 19:30:05 +0100

    Sat, 19:30, 1 file, M1 iMac
     M zsh/terminal_utils.zsh

--- zsh/terminal_utils.zsh
+++ zsh/terminal_utils.zsh
@@ -31,28 +31,9 @@ function sw () {
 	fi
 }
 
-function restart-terminal {
-	if [[ "$TERM_PROGRAM" == "Terminus-Sublime" ]] ; then
-		echo "Terminal restart not configured yet for Terminus."
-		return 1
-	fi
-
-	# some Terminals (e.g. alacritty) do not register to TERM_PROGRAM,
-	# others like Terminus do not register to TERM properly.
-	if [[ "$TERM_PROGRAM" == "" ]]; then
-		TO_RESTART="$TERM"
-	else
-		TO_RESTART="$TERM_PROGRAM"
-	fi
-
-	osascript -e "tell application \"$TO_RESTART\" to quit"
-	sleep 0.4
-	open -a "$TO_RESTART"
-}
-
+# restart terminal
 function rrr () {
-	# nohup "$ZSH_DOTFILE_LOCATION"/restart_terminal.zsh >/dev/null &
-	nohup restart-terminal >/dev/null &
+	nohup "$ZSH_DOTFILE_LOCATION"/restart_terminal.zsh >/dev/null &
 }
 
 # get path of file
