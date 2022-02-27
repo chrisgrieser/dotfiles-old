310046818242e9ef2c663e27606be5819bbaa981
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-27 13:30:03 +0100

    Sun, 13:30, 1 file, M1 iMac
     M zsh/run_last_before_login.zsh

--- zsh/run_last_before_login.zsh
+++ zsh/run_last_before_login.zsh
@@ -1,5 +1,10 @@
 if [[ "$TERM_PROGRAM" != "Terminus-Sublime" ]] ; then
-	bash "$ZSH_DOTFILE_LOCATION"/plugins/colorscript.bash --random
-	echo
-	fortune
+	# 33% art, 66% fortune
+	if [[ $((RANDOM%3)) == 1 ]] ; then
+		bash "$ZSH_DOTFILE_LOCATION"/plugins/colorscript.bash --random
+	else
+		echo "> $(fortune -n300 -s)"
+		echo
+	fi
 fi
+
