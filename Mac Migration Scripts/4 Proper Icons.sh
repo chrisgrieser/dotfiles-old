cb2166ebb2312d9e133fd85573cfebeca2abc609
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-01 17:30:05 +0100

    Tue, 17:30, 1 file, M1 iMac
     M "Mac Migration Scripts/4 Proper Icons.sh"

--- Mac Migration Scripts/4 Proper Icons.sh
+++ Mac Migration Scripts/4 Proper Icons.sh
@@ -32,9 +32,6 @@ touch '/Applications/Alacritty.app'
 # Sublime
 cp "$ICON_FOLDER"'Sublime Text Brown.icns' '/Applications/Sublime Text.app/Contents/Resources/Sublime Text.icns'
 touch '/Applications/Sublime Text.app'
-# Chromium
-cp "$ICON_FOLDER"'Chromium Gray.icns' '/Applications/Chromium.app/Contents/Resources/app.icns'
-touch '/Applications/Chromium.app'
 # Vivaldi
 cp "$ICON_FOLDER"'Vivaldi Red.icns' '/Applications/Vivaldi.app/Contents/Resources/app.icns'
 touch '/Applications/Vivaldi.app'
