07e7147ea124f19a1e3682f9c80cd82ee104ebbd
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-04 14:17:52 +0100

    patch

--- Pandoc/pandoc.zsh
+++ Pandoc/pandoc.zsh
@@ -1,16 +1,16 @@
 # shellcheck disable=SC2215
 
 function pd () {
-	INPUT_PATH="$1"
+	INPUT_PATH="$*"
 	OUTPUT_PATH="${INPUT_PATH%.*}.docx"
 	R_PATH="$(dirname "$INPUT_PATH")/attachments"
 
 	pandoc \
-		"$INPUT" \
-		-M date="$(date "+%e. %B %Y")" \
+		"$INPUT_PATH" \
 		--defaults="$ZSH_DOTFILE_LOCATION/../pandoc/pandoc-defaults.yml" \
 		--output="$OUTPUT_PATH" \
-		--resource-path="$R_PATH"
+		--resource-path="$R_PATH" \
+		-M date="$(date "+%e. %B %Y")"
 }
 
 # Pandoc Completions, https://groups.google.com/g/pandoc-discuss/c/Ot019yRiJFQ/m/VPchuJRkBQAJ
