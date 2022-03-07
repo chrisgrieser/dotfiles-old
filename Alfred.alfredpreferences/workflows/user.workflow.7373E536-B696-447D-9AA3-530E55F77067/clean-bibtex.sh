fde91312a325cd1cc29cedc60f33426cba98d61b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-07 17:30:05 +0100

    Mon, 17:30, 3 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.7373E536-B696-447D-9AA3-530E55F77067/clean-bibtex.sh
     M Alfred.alfredpreferences/workflows/user.workflow.7373E536-B696-447D-9AA3-530E55F77067/info.plist
     M pandoc/defaults/Paper-PDF.yaml

--- Alfred.alfredpreferences/workflows/user.workflow.7373E536-B696-447D-9AA3-530E55F77067/clean-bibtex.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.7373E536-B696-447D-9AA3-530E55F77067/clean-bibtex.sh
@@ -1,23 +1,12 @@
 #!/bin/zsh
 
-
 LIBRARY_PATH="${LIBRARY_PATH/#\~/$HOME}"
 
-grep -vw "month =" "$LIBRARY_PATH" > ~/temp.bib
-mv ~/temp.bib "$LIBRARY_PATH"
-
-grep -vw "issn =" "$LIBRARY_PATH" > ~/temp.bib
-mv ~/temp.bib "$LIBRARY_PATH"
-
-grep -vw "file =" "$LIBRARY_PATH" > ~/temp.bib
-mv ~/temp.bib "$LIBRARY_PATH"
+# shellcheck disable=SC2154
+grep -vw "month =" "$LIBRARY_PATH" | grep -vw "issn =" | grep -vw "file =" > "$alfred_workflow_cache"/temp.bib
+mv "$alfred_workflow_cache"/temp.bib "$LIBRARY_PATH"
 
 # fixes publication dates and journal for import of BibLaTeX from Zotero to BibDesk
-sed -E 's/date = {([[:digit:]]{4}).*}/year = {\1}/g' "$LIBRARY_PATH" > ~/temp.bib
-mv ~/temp.bib "$LIBRARY_PATH"
-sed -E 's/journaltitle = /journal = /g' "$LIBRARY_PATH" > ~/temp.bib
-mv ~/temp.bib "$LIBRARY_PATH"
-sed -E 's/note = /annotate = /g' "$LIBRARY_PATH" > ~/temp.bib
-mv ~/temp.bib "$LIBRARY_PATH"
-
-echo -n "{query}"
+sed -E -i '' 's/date = {([[:digit:]]{4}).*}/year = {\1}/g' "$LIBRARY_PATH"
+sed -i '' 's/journaltitle = /journal = /g' "$LIBRARY_PATH"
+sed -i '' 's/note = /annotate = /g' "$LIBRARY_PATH"
