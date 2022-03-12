3fc6b9f58a7f12ff371ba1397a726e985594d1bf
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-12 21:00:04 +0100

    Sat, 21:00, 4 files, M1 iMac
     M "Sublime User Folder/Default (OSX).sublime-keymap"
     M "Utility Scripts/clean-bibtex.sh"
     D pandoc/main-bibliography-1.bib
     M pandoc/main-bibliography.bib

--- Utility Scripts/clean-bibtex.sh
+++ Utility Scripts/clean-bibtex.sh
@@ -1,24 +1,30 @@
 #!/bin/zsh
+# shellcheck disable=SC2002
 
 LIBRARY_PATH=~"/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Configs/pandoc/main-bibliography.bib"
 
+# move curly braces down a line for better editing
+cat "$LIBRARY_PATH" | tr "\n" "§" | sed 's/}}§§@/}§}§§@/g'| tr "§" "\n" > ./temp.bib
+mv ./temp.bib "$LIBRARY_PATH"
+
+# remove unused fields
 grep -vw "month =" "$LIBRARY_PATH" \
 	| grep -vw "issn =" \
 	| grep -vw "file =" \
 	| grep -vw "langid =" \
 	| grep -vw "urlyear =" \
-	| grep -vw "bdsk-url-1 =" \
-	| grep -vw "bdsk-url-2 =" \
 	| grep -vw "abstract =" \
 	| grep -vw "shorttitle =" \
 	| grep -vw "shortjournal =" \
 	> ./temp.bib
 mv ./temp.bib "$LIBRARY_PATH"
 
-# shellcheck disable=SC2002
-cat "$LIBRARY_PATH" | tr "\n" "§" | sed 's/}}§§@/}§}§§@/g'| tr "§" "\n" > ./temp.bib
+cat "$LIBRARY_PATH" | tr "\n" "§" | sed 's/,§}/§}/g'| tr "§" "\n" > ./temp.bib
 mv ./temp.bib "$LIBRARY_PATH"
 
+# fix common problems
 sed -E -i '' 's/date = {([[:digit:]]{4}).*}/year = {\1}/g' "$LIBRARY_PATH"
 sed -i '' 's/journaltitle = /journal = /g' "$LIBRARY_PATH"
 sed -i '' 's/note = /annotate = /g' "$LIBRARY_PATH"
+
+# in some cases, the last entry must be fixed manually
