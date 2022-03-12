6602869f28f86b04863e441bd48919e97b1ca38e
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-12 20:30:04 +0100

    Sat, 20:30, 3 files, M1 iMac
     M "Utility Scripts/clean-bibtex.sh"
     M pandoc/main-bibliography.bib
    ?? pandoc/main-bibliography-1.bib

--- Utility Scripts/clean-bibtex.sh
+++ Utility Scripts/clean-bibtex.sh
@@ -5,11 +5,18 @@ LIBRARY_PATH=~"/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Configs/p
 grep -vw "month =" "$LIBRARY_PATH" \
 	| grep -vw "issn =" \
 	| grep -vw "file =" \
-	| grep -vw "bdsk-file-1 =" \
-	| grep -vw "bdsk-file-2 =" \
+	| grep -vw "langid =" \
+	| grep -vw "urlyear =" \
 	| grep -vw "bdsk-url-1 =" \
 	| grep -vw "bdsk-url-2 =" \
+	| grep -vw "abstract =" \
+	| grep -vw "shorttitle =" \
+	| grep -vw "shortjournal =" \
 	> ./temp.bib
+mv ./temp.bib "$LIBRARY_PATH"
+
+# shellcheck disable=SC2002
+cat "$LIBRARY_PATH" | tr "\n" "§" | sed 's/}}§§@/}§}§§@/g'| tr "§" "\n" > ./temp.bib
 mv ./temp.bib "$LIBRARY_PATH"
 
 sed -E -i '' 's/date = {([[:digit:]]{4}).*}/year = {\1}/g' "$LIBRARY_PATH"
