e154de1c038c2a42c4edebddc8fd492f04d65be2
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-13 21:30:04 +0100

    Sun, 21:30, 1 file, M1 iMac
     M "Utility Scripts/Obsidian Opener/Obsidian Opener Part 1.js"

--- Utility Scripts/Obsidian Opener/Obsidian Opener Part 1.js
+++ Utility Scripts/Obsidian Opener/Obsidian Opener Part 1.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env osascript -l JavaScript
 
 function run(input) {
+	// config
+	const markdownApp = "Sublime Text";
+
+	// -----------------------
 	const app = Application.currentApplication();
 	app.includeStandardAdditions = true;
 
@@ -11,20 +15,18 @@ function run(input) {
 	const toOpen = [];
 	const pathArray = input.toString().split(",");
 
-	// if Obsi is frontmost, open in other app,
-	// to allow compatibility with "Open with Default app" core plugin
+	const isFileInObsidianVault = Object.values(vaults)
+		.some(v => pathArray[0].startsWith(v.path));
+	// if Obsi is frontmost, open in other app, to allow compatibility
+	// with "Open with Default app" core plugin
 	const obsidianIsFrontmost = Application("Obsidian").frontmost();
-	if (obsidianIsFrontmost) return ["false", ...pathArray ]
+	// relevant for '.obsidian' and '.trash'
+	const isInHiddenFolder = pathArray[0].includes("/.");
 
-	pathArray.forEach(filePath => {
-		const isFileInObsidianVault = Object.values(vaults).some(v => filePath.startsWith(v.path));
-		if (isFileInObsidianVault && !filePath.includes("/.")) { // second condition prevents the opening of files in .obsidian or .trash
-			toOpen.push("obsidian://open?path=" + encodeURIComponent(filePath));
-			openInObsi = true;
-		}
-		else toOpen.push(filePath);
-	});
-
-	return [openInObsi.toString(), ...toOpen];
-
+	// eslint-disable-next-line curly
+	if (isFileInObsidianVault && !isInHiddenFolder && !obsidianIsFrontmost) {
+		app.openLocation ("obsidian://open?path=" + encodeURIComponent(pathArray[0]));
+	} else {
+		app.doShellScript ("open -a '" + markdownApp + pathArray.join(" "))
+	}
 }
