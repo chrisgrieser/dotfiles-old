ca303d84c20ac4a942891e495b64b05a9c1bc1c2
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-15 11:30:04 +0100

    Tue, 11:30, 9 files, M1 iMac
     D .config/fish/conf.d/00_fig_pre.fish
     D .config/fish/conf.d/99_fig_post.fish
     D .config/starship.toml
     D .config/starship_terminus.toml
     D .config/thefuck/__pycache__/settings.cpython-310.pyc
     D .config/thefuck/settings.py
     M zsh/load_plugins.zsh
    ?? .config/starship/starship.toml
    ?? .config/starship/starship_terminus.toml

--- .config/fish/conf.d/00_fig_pre.fish
+++
@@ -1,1 +0,0 @@
-/Users/chrisgrieser/.fig/shell/pre.fish
--- zsh/load_plugins.zsh
+++ zsh/load_plugins.zsh
@@ -3,23 +3,23 @@
 # activate completions, also needed for ZSH auto suggestions & completions
 autoload compinit -Uz +X && compinit
 
-# NOTE: fzf-tab needs to be loaded after compinit, but before plugins which will wrap widgets, such as zsh-autosuggestions or fast-syntax-highlighting!!
+# NOTE: fzf-tab needs to be loaded after compinit, but before plugins which will
+# wrap widgets, such as zsh-autosuggestions or fast-syntax-highlighting!
 source "$ZSH_DOTFILE_LOCATION"/plugins/fzf-tab/fzf-tab.plugin.zsh
 
 # `brew --prefix` ensures the right path is inserted on M1 and non-M1 macs
 source "$(brew --prefix)"/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source "$(brew --prefix)"/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-eval "$(fasd --init auto)"
 eval "$(starship init zsh)"
 source "$ZSH_DOTFILE_LOCATION"/plugins/magic_enter.zsh
 source "$ZSH_DOTFILE_LOCATION"/plugins/obsidian-vault-navigation.sh
 
-# -------------------------------------------------
-# fix for Starship-Terminus issue
-# https://github.com/starship/starship/issues/3627
-# https://github.com/randy3k/Terminus/issues/344
+
 
 if [[ "$TERM_PROGRAM" == "Terminus-Sublime" ]] ; then
-	export STARSHIP_CONFIG=~/.config/starship_terminus.toml
+	# fix for Starship-Terminus issue, https://github.com/starship/starship/issues/3627
+	export STARSHIP_CONFIG=~/.config/starship/starship_terminus.toml
+else
+	export STARSHIP_CONFIG=~/.config/starship/starship.toml
 fi
