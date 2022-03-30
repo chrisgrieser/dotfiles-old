325109e97c103e176165d02e4f71efb0baed0cbb
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-30 18:30:04 +0200

    Wed, 18:30, 4 files, M1 iMac
     M "Sublime User Folder/SublimeLinter.sublime-settings"
     M pandoc/main-bibliography.bib
     M zsh/aliases.zsh
    ?? pandoc/main-bibliography.bib.bak

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -40,7 +40,7 @@ alias pip="pip3"
 
 # exa
 alias exa='exa --all --icons --group-directories-first --sort=modified --ignore-glob=.DS_Store'
-alias exagit='git status --short ; exa --long --grid --git --git-ignore --no-user --no-permissions --no-time --no-filesize --ignore-glob=.git ; echo ""'
+alias exagit='git status --short ; exa --long --grid --git --git-ignore --no-user --no-permissions --no-time --no-filesize --ignore-glob=.git'
 alias tree='exa --tree -L2'
 alias tree2='exa --tree -L2'
 alias tree3='exa --tree -L3'
