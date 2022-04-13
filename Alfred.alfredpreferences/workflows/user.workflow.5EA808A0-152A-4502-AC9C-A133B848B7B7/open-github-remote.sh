4d53f4dcfd2ba3ae088b2e115c31eafbcfcedf0d
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-13 14:30:04 +0200

    Wed, 14:30, 2 files, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/open-github-remote.sh

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/open-github-remote.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/open-github-remote.sh
@@ -1,19 +1,24 @@
 #!/bin/zsh
 
-# get path of current Finder Window
-finderPath=$(osascript -e 'tell application "Finder"
-	if (count windows) is 0 then
-		set pathToOpen to (path to home folder as string)
-	else
-		set pathToOpen to target of Finder window 1 as alias
+# get path of current Finder Selection/Window
+FINDER_SELECTION=$(osascript -e 'tell application "Finder"
+	set sel to selection
+	if ((count sel) > 1) then
+		POSIX path of ((item 1 of sel) as text)
+	else if ((count sel) = 1) then
+		return POSIX path of (sel as text)
+	else if (count windows) is 0 then
+		return "no window"
 	end if
-end tell
-return (POSIX path of pathToOpen)')
+	return POSIX path of (target of window 1 as alias)
+end tell')
+
 cd "$finderPath" || return
 
 # go to to git root https://stackoverflow.com/a/38843585
 # shellcheck disable=SC2164
 r=$(git rev-parse --git-dir) && r=$(cd "$r" && pwd)/ && cd "${r%%/.git/*}"
+
 
 # open at GitHub
 open "$(git remote -v | grep git@github.com | grep fetch | head -n1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/https:\/\//' -e 's/\.git//')"
