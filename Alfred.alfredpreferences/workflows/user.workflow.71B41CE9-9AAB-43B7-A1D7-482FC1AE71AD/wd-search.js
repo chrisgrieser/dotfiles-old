a80f67898737e7ea1a41a65a9a8049035a961ad9
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-16 21:00:04 +0200

    Sat, 21:00, 1 file, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.71B41CE9-9AAB-43B7-A1D7-482FC1AE71AD/wd-search.js

--- Alfred.alfredpreferences/workflows/user.workflow.71B41CE9-9AAB-43B7-A1D7-482FC1AE71AD/wd-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.71B41CE9-9AAB-43B7-A1D7-482FC1AE71AD/wd-search.js
@@ -9,14 +9,26 @@ const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
 const defaultFolder = $.getenv("default_folder").replace(/^~/, app.pathTo("home folder"));
 
 const workArray = app.doShellScript (`ls -1 '${defaultFolder}'`)
-	.split("\r");
+	.split("\r")
+	.map(item => {
+		const itemPath = defaultFolder + "/" + item;
 
-workArray.map(item => {
-	return {
-		"title": item,
-		"match": alfredMatcher (item),
-		"arg": defaultFolder + "/" + item,
-	};
-});
+		let iconToDisplay;
+		if (item.endsWith(".png")) iconToDisplay = { "path": itemPath };
+		else {
+			iconToDisplay = {
+				"type": "fileicon",
+				"path": itemPath
+			};
+		}
+
+		return {
+			"title": item,
+			"match": alfredMatcher (item),
+			"type": "file:skipcheck",
+			"arg": itemPath,
+			"icon": iconToDisplay,
+		};
+	});
 
 JSON.stringify({ items: workArray });
