19db42c6b8946c5ce9fc7a2c40f19c55ec164711
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-16 14:00:35 +0200

    Sat, 14:00, 1 file, M1 iMac
     M Alfred.alfredpreferences/workflows/user.workflow.16DF7392-3E39-4507-BB82-EB4C1D5035D0/better-screenshot.sh

--- Alfred.alfredpreferences/workflows/user.workflow.16DF7392-3E39-4507-BB82-EB4C1D5035D0/better-screenshot.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.16DF7392-3E39-4507-BB82-EB4C1D5035D0/better-screenshot.sh
@@ -1,11 +1,16 @@
 #!/bin/zsh
-temp_image="$alfred_workflow_cache""/Screenshot ""$(date '+%Y-%m-%d %H.%M.%S')"".png"
+# shellcheck disable=SC2154
+
+# required base name for the image renamer plugin to work with it
+IMAGE_BASE_NAME="Pasted Image"
+
+TEMP_IMAGE="$alfred_workflow_cache/$IMAGE_BASE_NAME $(date '+%Y-%m-%d %H.%M.%S').png"
 mkdir -p "$alfred_workflow_cache"
 
-screencapture -i "$temp_image"
+screencapture -i "$TEMP_IMAGE"
 
 # existence check allows for the canceling of a screenshot
-if [[ -e "$temp_image" ]] ; then
-	osascript -e "set the clipboard to POSIX file \"$temp_image\""
+if [[ -e "$TEMP_IMAGE" ]] ; then
+	osascript -e "set the clipboard to POSIX file \"$TEMP_IMAGE\""
 	echo "image done"
 fi
