31c50af2d2747fde3544784c93a258d1c8d19b52
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-09 19:00:02 +0200

    Mon, 19:00, 4 files, M1 iMac
     M "Sublime User Folder/Preferences.sublime-settings"
     D "Sublime User Folder/StatusBarFileSize.sublime-settings"
     M "Sublime User Folder/one-tab-no-tabbar.py"
     D "Sublime User Folder/syntax_fold.sublime-settings"

--- Sublime User Folder/one-tab-no-tabbar.py
+++ Sublime User Folder/one-tab-no-tabbar.py
@@ -1,18 +1,20 @@
+# by @UlraInstinct05 on Discord
 import sublime
 import sublime_plugin
 
 class RemoveTabBarListener(sublime_plugin.EventListener):
 
-	def on_load_async(self, view):
-		self.close_tab_bar(view, len(view.window().views()))
+		def on_load_async(self, view):
+				self.close_tab_bar(view, len(view.window().views()))
 
 		def on_pre_close(self, view):
-			self.close_tab_bar(view, len(view.window().views()) - 1)
+				self.close_tab_bar(view, len(view.window().views()) - 1)
 
-			def close_tab_bar(self, view, num_views):
+		@staticmethod
+		def close_tab_bar(view, num_views):
 				if num_views == 1:
-					view.window().set_tabs_visible(False)
+						view.window().set_tabs_visible(False)
 				else:
-					view.window().set_tabs_visible(True)
+						view.window().set_tabs_visible(True)
 
-					on_load = on_new = on_new_async = on_load_async
+		on_load = on_new = on_new_async = on_load_async
