7040eea7586026c7745f2ec246b2c9b3412e5893
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-13 14:30:05 +0200

    Fri, 14:30, 3 files, M1 iMac
     M "Sublime User Folder/Package Control.sublime-settings"
     M "Sublime User Folder/important_switcher.py"
    ?? .flake8

--- Sublime User Folder/important_switcher.py
+++ Sublime User Folder/important_switcher.py
@@ -1,14 +1,13 @@
-import sublime
 import sublime_plugin
-import re
+# import re
 
 
 class ImportantSwitcher(sublime_plugin.TextCommand):
 	def run(self, edit):
-		for region in self.view.sel(): # for every selection (region = range in Sublime)
-			line = self.view.line(region.begin()) # get line that contains the beginning of the region
-			lineContent = self.view.substr(line) # get content of line
+		for region in self.view.sel():  # for every selection (region = range in Sublime)
+			line = self.view.line(region.begin())  # get line that contains the beginning of the region
+			lineContent = self.view.substr(line)  # get content of line
 
-			lineContent = "bla" # placeholder; do line manipulation
+			lineContent = "bla"  # placeholder; do line manipulation
 
-			self.view.replace(edit, line, lineContent) # replace line with new content
+			self.view.replace(edit, line, lineContent)  # replace line with new contentbla
