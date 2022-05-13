ca6e13f10377babcb2b8cf08015ba9df8d25edba
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-13 15:43:17 +0200

    patch

--- Sublime User Folder/important_switcher.py
+++ Sublime User Folder/important_switcher.py
@@ -5,12 +5,12 @@ import sublime_plugin
 class ImportantSwitcher(sublime_plugin.TextCommand):  # pylint: disable=too-few-public-methods
 	def run(self, edit):
 		for region in self.view.sel():  # for every selection (region = range in Sublime)
-			line = self.view.line(region.begin())  # get line that contains the beginning of the region
-			line_content = self.view.substr(line)  # get content of line
+			line = self.view.line(region.begin())  # get line that contains the beginning of the selection
+			line_content = self.view.substr(line)
 
-			if line_content.find("!important"):
-				line_content = re.sub(r"\s?!important", "", line_content)
+			if "!important" in line_content:
+				line_content = re.sub(r"\s?!important", "", line_content, 1)
 			else:
-				line_content = re.sub(r"\s?!important", "!important", line_content, 0, re.MULTILINE)
+				line_content = re.sub(";?$", " !important;", line_content, 1)
 
-			self.view.replace(edit, line, line_content)  # replace line with new contentbla
+			self.view.replace(edit, line, line_content)
