1e490746c3261a27cbec2f1a9122afdde400bb4a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-13 16:30:04 +0200

    Fri, 16:30, 1 file, M1 iMac
     M "Sublime User Folder/important_switcher.py"

--- Sublime User Folder/important_switcher.py
+++ Sublime User Folder/important_switcher.py
@@ -5,12 +5,12 @@ import sublime_plugin
 class ImportantSwitcher(sublime_plugin.TextCommand):  # pylint: disable=too-few-public-methods
 	def run(self, edit):
 		for region in self.view.sel():  # for every selection (region = range in Sublime)
-			line = self.view.line(region.begin())  # get line that contains the beginning of the selection
+			line = self.view.line(region)  # extend to beginning/end of line
 			line_content = self.view.substr(line)
 
 			if "!important" in line_content:
-				line_content = re.sub(r"\s?!important", "", line_content, 1)
+				line_content = re.sub(r"\s?!important", "", line_content, 0)
 			else:
-				line_content = re.sub(";?$", " !important;", line_content, 1)
+				line_content = re.sub(";$", " !important;", line_content, 0, re.MULTILINE)
 
 			self.view.replace(edit, line, line_content)
