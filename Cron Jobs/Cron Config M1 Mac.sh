b0bf9f655fa1706767dec4554f425b51bc7ec707
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-13 20:30:04 +0200

    Fri, 20:30, 2 files, M1 iMac
     M "Cron Jobs/Cron Config M1 Mac.sh"
    ?? "Cron Jobs/daily-evening.applescript"

--- Cron Jobs/Cron Config M1 Mac.sh
+++ Cron Jobs/Cron Config M1 Mac.sh
@@ -13,7 +13,9 @@ function add-cronjob () {
 
 add-cronjob "*/30 * * * *" '30-min_[Browser-Path].sh'
 add-cronjob "0 3 * * *" sleep-timer.applescript
-add-cronjob "5 6 * * *" 'daily-morning_[Browser].applescript'
+add-cronjob "0 6 * * *" 'daily-morning_[Browser].applescript'
+add-cronjob "0 21 * * *" daily-evening.applescript
 add-cronjob "5 6 * * 1,4" biweekly.applescript
+
 echo ""
 crontab -l # check the current cronjobs
---
+++ Cron Jobs/daily-evening.applescript
@@ -0,0 +1,10 @@
+#!/usr/bin/env osascript
+
+# SWITCH TO DARK MODE
+# only when not dark mode already
+tell application "System Events"
+	tell appearance preferences
+		if (dark mode is false) then set dark mode to true
+		tell application id "com.runningwithcrayons.Alfred" to run trigger "toggle-dark-mode" in workflow "com.sirshanksalot.dark-mode-toggle"
+	end tell
+end tell
