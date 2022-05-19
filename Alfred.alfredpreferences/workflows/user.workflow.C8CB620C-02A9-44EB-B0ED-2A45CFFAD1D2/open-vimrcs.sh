af8ed743d396f4a2294c29c5217bcb1c66708078
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-19 18:30:04 +0200

    Thu, 18:30, 4 files,
     M Alfred.alfredpreferences/workflows/user.workflow.C8CB620C-02A9-44EB-B0ED-2A45CFFAD1D2/open-vimrcs.sh
     M "Sublime User Folder/Default (OSX).sublime-keymap"
     D "Sublime User Folder/vimrcs.sublime-project"
    ?? "Sublime Projects/vimrcs.sublime-project"

--- Alfred.alfredpreferences/workflows/user.workflow.C8CB620C-02A9-44EB-B0ED-2A45CFFAD1D2/open-vimrcs.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.C8CB620C-02A9-44EB-B0ED-2A45CFFAD1D2/open-vimrcs.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH
-subl --project ~"/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Configs/Sublime User Folder/vimrcs.sublime-project"
+subl --project ~"/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Configs/Sublime Projects/vimrcs.sublime-project"
 
 osascript -e "tell application \"System Events\" to keystroke space using {command down, option down, shift down, control down}"
+
