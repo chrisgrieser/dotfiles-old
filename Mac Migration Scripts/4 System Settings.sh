591afa605f2aef3d8951b3c0d9541fb8d3e4ec0f
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-25 11:30:04 +0200

    Wed, 11:30, 4 files,
     M .config/alacritty/alacritty.yml
     M .config/karabiner/assets/complex_modifications/misc.json
     M .config/karabiner/karabiner.json
     M "Mac Migration Scripts/4 System Settings.sh"

--- Mac Migration Scripts/4 System Settings.sh
+++ Mac Migration Scripts/4 System Settings.sh
@@ -78,7 +78,7 @@ defaults write com.apple.dock mouse-over-hilite-stack -bool false
 defaults write com.apple.dock minimize-to-application -bool false
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 defaults write com.apple.dock show-process-indicators -bool true
-defaults write com.apple.dock static-only -bool true # show only active items in dock
+# defaults write com.apple.dock static-only -bool true # show only active items in dock
 
 # Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
