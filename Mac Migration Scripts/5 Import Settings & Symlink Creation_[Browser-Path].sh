4e9408b225fcb6b6ffcd281709c7c5fa543c3fab
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-25 16:30:03 +0200

    Wed, 16:30, 10 files,
     M Alfred.alfredpreferences/preferences/features/defaultresults/prefs.plist
     M Alfred.alfredpreferences/preferences/local/d91981849c74a5c72ca45c7cc69d0275828a55c1/features/defaultresults/prefs.plist
     D "Alfred.alfredpreferences/workflows/user.workflow.1543780E-4271-4CF4-9956-DFBFD1C536D3/Word to PDF.js"
     D Alfred.alfredpreferences/workflows/user.workflow.1543780E-4271-4CF4-9956-DFBFD1C536D3/icon.afdesign
     D Alfred.alfredpreferences/workflows/user.workflow.1543780E-4271-4CF4-9956-DFBFD1C536D3/icon.png
     D Alfred.alfredpreferences/workflows/user.workflow.1543780E-4271-4CF4-9956-DFBFD1C536D3/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.C19E0A76-5D58-43C9-AB2D-EF75CC57629B/info.plist
     M "Mac Migration Scripts/5 Import Settings & Symlink Creation_[Browser-Path].sh"
     M "Sublime User Folder/Terminus.hidden-color-scheme"
     M "Sublime User Folder/Terminus/Terminus.hidden-color-scheme"

--- Mac Migration Scripts/5 Import Settings & Symlink Creation_[Browser-Path].sh
+++ Mac Migration Scripts/5 Import Settings & Symlink Creation_[Browser-Path].sh
@@ -19,36 +19,41 @@ mv -vR 'Browser-Default-Folder/'* ~"/Library/Application Support/BraveSoftware/B
 # -----------
 
 # Symlink Dotfiles
+DOTFILE_FOLDER=~"/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Dotfiles/"
 mv ~/.zshrc ~/.Trash
-ln -s "$DOTFILE_FOLDER/zsh/.zshrc" ~
+ln -sf "$DOTFILE_FOLDER/zsh/.zshrc" ~
 mv ~/.zprofile ~/.Trash
-ln -s "$DOTFILE_FOLDER/zsh/.zprofile" ~
-ln -s "$DOTFILE_FOLDER/zsh/.zlogin" ~
+ln -sf "$DOTFILE_FOLDER/zsh/.zprofile" ~
+ln -sf "$DOTFILE_FOLDER/zsh/.zlogin" ~
 
-ln -s "$DOTFILE_FOLDER/.searchlink" ~
-ln -s "$DOTFILE_FOLDER/.shellcheckrc" ~
-ln -s "$DOTFILE_FOLDER/.stylelintrc.json" ~
-ln -s "$DOTFILE_FOLDER/.gitconfig" ~
-ln -s "$DOTFILE_FOLDER/.eslintrc.json" ~
-ln -s "$DOTFILE_FOLDER/.gitignore_global" ~
-ln -s "$DOTFILE_FOLDER/pandoc" ~/.pandoc
-ln -s "$DOTFILE_FOLDER/.markdownlintrc" ~
+ln -sf "$DOTFILE_FOLDER/.searchlink" ~
+ln -sf "$DOTFILE_FOLDER/.shellcheckrc" ~
+ln -sf "$DOTFILE_FOLDER/.stylelintrc.json" ~
+ln -sf "$DOTFILE_FOLDER/.gitconfig" ~
+ln -sf "$DOTFILE_FOLDER/.eslintrc.json" ~
+ln -sf "$DOTFILE_FOLDER/.gitignore_global" ~
+ln -sf "$DOTFILE_FOLDER/pandoc" ~/.pandoc
+ln -sf "$DOTFILE_FOLDER/.markdownlintrc" ~
 
-ln -s "$DOTFILE_FOLDER/.pylintrc" ~
-ln -s "$DOTFILE_FOLDER/.flake8" ~
+ln -sf "$DOTFILE_FOLDER/.pylintrc" ~
+ln -sf "$DOTFILE_FOLDER/.flake8" ~
 
-ln -s "$DOTFILE_FOLDER/.vimrc" ~
-ln -s "$DOTFILE_FOLDER/obsidian.vimrc" "$VAULT_PATH/Meta"
+ln -sf "$DOTFILE_FOLDER/.vimrc" ~
+ln -sf "$DOTFILE_FOLDER/obsidian.vimrc" "$VAULT_PATH/Meta"
 
 # .config
 mv ~/.config ~/.Trash
-ln -s "$DOTFILE_FOLDER/.config/" ~/.config
+ln -sf "$DOTFILE_FOLDER/.config/" ~/.config
 
 # Sublime
 mv ~"/Library/Application Support/Sublime Text/Packages/User" ~/.Trash
-ln -s "$DOTFILE_FOLDER/Sublime User Folder/" ~"/Library/Application Support/Sublime Text/Packages/User"
+ln -sf "$DOTFILE_FOLDER/Sublime User Folder/" ~"/Library/Application Support/Sublime Text/Packages/User"
 mv ~"/Library/Application Support/Sublime Text/Installed Packages/CSS3.sublime-package" ~/.Trash
-ln -s "$DOTFILE_FOLDER/Sublime Packages/CSS3.sublime-package" ~"/Library/Application Support/Sublime Text/Installed Packages"
+ln -sf "$DOTFILE_FOLDER/Sublime Packages/CSS3.sublime-package" ~"/Library/Application Support/Sublime Text/Installed Packages"
+
+# Folder Actions
+mv -f ~"/Library/Workflows/Applications/Folder Actions" ~/.Trash
+ln -sf "$DOTFILE_FOLDER/Folder Actions/" ~"/Library/Workflows/Applications/Folder Actions"
 
 # -------------------
 # only on main device
@@ -61,10 +66,6 @@ ln -s "$DOTFILE_FOLDER/Dock and Launchpad" ~"/Library/Application Support/Dock"
 # Brave
 mv ~"/Applications/Brave Browser Apps.localized" ~/.Trash
 ln -s "$DOTFILE_FOLDER/../Brave Browser Apps.localized/" ~"/Applications/Brave Browser Apps.localized"
-
-# Folder Actions
-mv -f ~"/Library/Workflows/Applications/Folder Actions" ~/.Trash
-ln -s "$DOTFILE_FOLDER/Folder Actions/" ~"/Library/Workflows/Applications/Folder Actions"
 
 # VLC
 mv -f ~"/Library/Preferences/org.videolan.vlc/vlcrc" ~/.Trash
