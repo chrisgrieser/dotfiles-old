e8990f6dad4e8b8e5453ac9ad423753902f3a8d8
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-31 17:00:05 +0200

    Tue, 17:00, 6 files,
     D "Mac Migration Scripts/5 Import Settings & Symlink Creation_[Browser-Path].sh"
     D com.knollsoft.Charmstone.plist
     D "unused/vintage rebindings.json"
    ?? "Mac Migration Scripts/5 Import Settings_[Browser-Path].sh"
    ?? "Mac Migration Scripts/6 Symlink Creation.sh"
    ?? unused/com.knollsoft.Charmstone.plist

---
+++ Mac Migration Scripts/6 Symlink Creation.sh
@@ -0,0 +1,55 @@
+cd ~/Desktop || return
+
+# -------------------
+
+DOTFILE_FOLDER=~"/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Dotfiles/"
+mv ~/.zshrc ~/.Trash
+ln -sf "$DOTFILE_FOLDER/zsh/.zshrc" ~
+mv ~/.zprofile ~/.Trash
+ln -sf "$DOTFILE_FOLDER/zsh/.zprofile" ~
+ln -sf "$DOTFILE_FOLDER/zsh/.zlogin" ~
+
+ln -sf "$DOTFILE_FOLDER/.searchlink" ~
+ln -sf "$DOTFILE_FOLDER/.shellcheckrc" ~
+ln -sf "$DOTFILE_FOLDER/.stylelintrc.json" ~
+ln -sf "$DOTFILE_FOLDER/.gitconfig" ~
+ln -sf "$DOTFILE_FOLDER/.eslintrc.json" ~
+ln -sf "$DOTFILE_FOLDER/.gitignore_global" ~
+ln -sf "$DOTFILE_FOLDER/pandoc" ~/.pandoc
+ln -sf "$DOTFILE_FOLDER/.markdownlintrc" ~
+
+ln -sf "$DOTFILE_FOLDER/.pylintrc" ~
+ln -sf "$DOTFILE_FOLDER/.flake8" ~
+ln -sf "$DOTFILE_FOLDER/.vimrc" ~
+
+# .config
+mv ~/.config ~/.Trash
+ln -sf "$DOTFILE_FOLDER/.config/" ~/.config
+
+# Sublime
+mv ~"/Library/Application Support/Sublime Text/Packages/User" ~/.Trash
+ln -sf "$DOTFILE_FOLDER/Sublime User Folder/" ~"/Library/Application Support/Sublime Text/Packages/User"
+mv ~"/Library/Application Support/Sublime Text/Installed Packages/CSS3.sublime-package" ~/.Trash
+ln -sf "$DOTFILE_FOLDER/Sublime Packages/CSS3.sublime-package" ~"/Library/Application Support/Sublime Text/Installed Packages"
+
+# Folder Actions
+mv -f ~"/Library/Workflows/Applications/Folder Actions" ~/.Trash
+ln -sf "$DOTFILE_FOLDER/Folder Actions/" ~"/Library/Workflows/Applications/Folder Actions"
+
+# -------------------
+# not on all devices
+# -------------------
+# Obsidian vimrcs
+VAULT_PATH=~'/Library/Mobile Documents/iCloud~md~obsidian/Documents/Main Vault'
+ln -sf "$DOTFILE_FOLDER/obsidian.vimrc" "$VAULT_PATH/Meta"
+ln -sf "$DOTFILE_FOLDER/obsidian.vimrc" "$VAULT_PATH/../Development"
+
+# Brave
+mv ~"/Applications/Brave Browser Apps.localized" ~/.Trash
+ln -sf "$DOTFILE_FOLDER/../Brave Browser Apps.localized/" ~"/Applications/Brave Browser Apps.localized"
+
+# VLC
+mv -f ~"/Library/Preferences/org.videolan.vlc/vlcrc" ~/.Trash
+mkdir -p ~"/Library/Preferences/org.videolan.vlc/"
+ln -sf "$DOTFILE_FOLDER/vlcrc" ~"/Library/Preferences/org.videolan.vlc/vlcrc"
+
