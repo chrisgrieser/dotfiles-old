013408ea2921d0ada79b8aca011eb3f0e914eb6e
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-06 15:30:04 +0200

    Mon, 15:30, 2 files,
     M Alfred.alfredpreferences/workflows/user.workflow.91B36D72-790B-478E-8F75-A92D20F5F755/info.plist
     M espanso/scripts/alfred2espanso.sh

--- espanso/scripts/alfred2espanso.sh
+++ espanso/scripts/alfred2espanso.sh
@@ -1,17 +1,36 @@
 #!/bin/zsh
 
-# Alfred snippets to Espanso
+# Convert Alfred snippets to Espanso files
+# ----------------------------------------------
+TARGET="$WD"
+# path to your Alfred snippets
+SNIPPET_FOLDER="/Users/chrisgrieser/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Dotfiles/Alfred.alfredpreferences/snippets/"
 
-cd "/Users/chrisgrieser/Library/Mobile Documents/com~apple~CloudDocs/Dotfolder/Dotfiles/Alfred.alfredpreferences/snippets/"
-
+cd "$SNIPPET_FOLDER" || return 1
 
 for folder in */; do
-	cd "$folder"
+	cd "$folder" || return 1
 	echo "$PWD"
+
 	NAME=$(basename "$PWD")
 	for f in *.json ; do
 		f=$(basename "$f" .json)
-		yq -P '.' "$f.json" >> "$NAME.yml"
+		yq -P '.' "$f.json" >> "$TARGET/$NAME.yml"
 	done
+
 	cd ..
 done
+
+TARGET="$WD"
+cd "$TARGET" || return 1
+for file in *.yml; do
+	echo "$file"
+	sed -i '' '/alfredsnippet:/d' "$file"
+	sed -i '' '/uid:/d' "$file"
+	sed -i '' '/dontautoexpand:/d' "$file"
+	sed -i '' 's/snippet:/replace:/g' "$file"
+	sed -i '' 's/name:/label:/g' "$file"
+	sed -i '' 's/  keyword:/- trigger:/g' "$file"
+	sed -i '' 's/{cursor}/\$\|\$/g' "$file"
+done
+
