2c8163ce1ececfa554a5b07c3db45c5c615fcef9
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-22 23:00:03 +0200

    Wed, 23:00, 2 files, Chris-M1-iMac
     M "Cron Jobs/frequent.log"
     M hammerspoon/watchers.lua

--- hammerspoon/watchers.lua
+++ hammerspoon/watchers.lua
@@ -1,7 +1,7 @@
 -- https://www.hammerspoon.org/go/
 -------------------------------------
 
--- Finder: Bring all windows forward & hide sidebar when activated
+-- FINDER: Bring all windows forward & hide sidebar when activated
 function finderWatcher(appName, eventType, appObject)
 	if (eventType == hs.application.watcher.activated) then
 		if (appName == "Finder") then
@@ -13,7 +13,7 @@ end
 finderAppWatcher = hs.application.watcher.new(finderWatcher)
 finderAppWatcher:start()
 
--- Discord: Open OMG Server instead of Friends tab
+-- DISCORD: Open OMG Server instead of Friends tab
 function discordWatcher(appName, eventType)
 	if (eventType == hs.application.watcher.launching) then
 		if (appName == "Discord") then
@@ -24,30 +24,42 @@ end
 discordAppWatcher = hs.application.watcher.new(discordWatcher)
 discordAppWatcher:start()
 
--- Highlights: Sync Dark & Light Mode
+-- DRAFTS: Sidebar Toggle with Window Position
+function draftSideBars(appName, eventType, appObject)
+	if (eventType == hs.application.watcher.activated) then
+		if (appName == "Drafts") then
+			draftsWindow = hs.window.find("Drafts")
+			appObject:selectMenuItem({"View", "Hide Draft List"})
+		end
+	end
+end
+draftsAppWatcher = hs.application.watcher.new(draftSideBars)
+draftsAppWatcher:start()
+
+-- HIGHLIGHTS: Sync Dark & Light Mode
 function highlightsWatcher(appName, eventType)
 	if (eventType == hs.application.watcher.launching) then
 		if (appName == "Highlights") then
 			hs.applescript([[
-tell application "System Events"
-	tell appearance preferences to set isDark to dark mode
-	if (isDark is false) then set targetView to "Default"
-	if (isDark is true) then set targetView to "Night"
-	delay 0.5
+				tell application "System Events"
+					tell appearance preferences to set isDark to dark mode
+					if (isDark is false) then set targetView to "Default"
+					if (isDark is true) then set targetView to "Night"
+					delay 0.5
 
-	tell process "Highlights"
-		set frontmost to true
-		click menu item targetView of menu of menu item "PDF Appearance" of menu "View" of menu bar 1
-	end tell
-end tell
-]])
+					tell process "Highlights"
+						set frontmost to true
+						click menu item targetView of menu of menu item "PDF Appearance" of menu "View" of menu bar 1
+					end tell
+				end tell
+			]])
 		end
 	end
 end
 highlightsAppWatcher = hs.application.watcher.new(highlightsWatcher)
 highlightsAppWatcher:start()
 
--- Dim brightness when projector is connected
+-- PROJECTOR: Dim brightness when projector is connected
 function displayCountWatcher()
 	local screenName = hs.screen.primaryScreen():name()
 	if screenName == "ViewSonic PJ" then
