87fed9c02767c2c6fe88ec7fa297ba4416cb6f20
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-23 15:30:05 +0200

    Thu, 15:30, 2 files, Chris-M1-iMac
     M "Cron Jobs/frequent.log"
     M hammerspoon/menubar.lua

--- hammerspoon/menubar.lua
+++ hammerspoon/menubar.lua
@@ -1,27 +1,36 @@
 -- https://www.hammerspoon.org/docs/hs.menubar.html
 -- https://www.hammerspoon.org/go/#simplemenubar
---------------------------------------------------------------------------------
 require("utils")
+--------------------------------------------------------------------------------
+weatherUpdateMin = 15
+weatherLocation = "Berlin"
 
 function setWeather()
 	weatherStatusBar = hs.menubar.new()
-	local _, weather = hs.http.get("https://wttr.in/Berlin?format=1", nil)
+	local _, weather = hs.http.get("https://wttr.in/" .. weatherLocation .. "?format=1", nil)
 	weather = weather:gsub("\n", ""):gsub("+", "")
 	weatherStatusBar:setTitle(weather)
 end
 setWeather()
-
-updateIntervall = 15
-hs.timer.doEvery(updateIntervall * 60, setWeather)
+hs.timer.doEvery(weatherUpdateMin * 60, setWeather)
 
 --------------------------------------------------------------------------------
+covidUpdateHours = 12
+covidLocationCode = "BE"
+
 function setCovidBar()
 	covidBar = hs.menubar.new()
 	local _, dataJSON = hs.http.get("https://api.corona-zahlen.org/germany", nil)
 	local covidNumbers = hs.json.decode(dataJSON)
-	local sevenDayIncidence = covidNumbers.weekIncidence:gsub(".%d", "")
-
-	covidBar:setTitle("🦠" .. sevenDayIncidence)
+	local sevenDayIncidence = math.floor(covidNumbers.weekIncidence)
+	local r = covidNumbers.r.rValue7Days.value
+	covidBar:setTitle("🦠 " .. sevenDayIncidence .. " (" .. r ..  ")")
 end
 setCovidBar()
+hs.timer.doEvery(covidUpdateHours * 60 * 60, setCovidBar)
 
+-- JS for state numbers
+-- const stateID = "BE";
+-- const stateData = await getOnlineJson("https://api.corona-zahlen.org/states/" + stateID);
+-- const state7DInc = stateData.data[stateID].weekIncidence;
+-- const stateName = stateData.data[stateID].name
