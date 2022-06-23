5577ebd64bfae5a41ddcfcd9d82034f2736597e4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-23 15:00:04 +0200

    Thu, 15:00, 2 files, Chris-M1-iMac
     M "Cron Jobs/frequent.log"
     M hammerspoon/menubar.lua

--- hammerspoon/menubar.lua
+++ hammerspoon/menubar.lua
@@ -4,13 +4,14 @@
 
 require("utils")
 
-weatherStatusBar = hs.menubar.new()
 function setWeather()
+	weatherStatusBar = hs.menubar.new()
 	local _, weather = hs.http.get("https://wttr.in/Berlin?format=1", nil)
+	weather = weather:gsub("\n", ""):gsub("+", "")
 	weatherStatusBar:setTitle(weather)
 end
 setWeather()
 
-
-
-
+updateIntervall = 15
+intervalSecs = hs.timer.seconds(tostring(updateIntervall) .. "m")
+hs.timer.doEvery(intervalSecs, setWeather)
