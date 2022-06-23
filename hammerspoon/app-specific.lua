af625a8c5389f554b0c6bf1f528fcf8a06c9a92d
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-23 16:15:04 +0200

    Thu, 16:15, 3 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.72A3C5CD-5F62-4822-827A-718F7BD20E99/info.plist
     M "Cron Jobs/frequent.log"
     M hammerspoon/app-specific.lua

--- hammerspoon/app-specific.lua
+++ hammerspoon/app-specific.lua
@@ -1,13 +1,24 @@
 -- https://www.hammerspoon.org/docs/hs.eventtap.html
 function twitterrificScrollUp ()
-	local frontapp = hs.application.frontmostApplication():name()
+	local previousApp = hs.application.frontmostApplication():name()
 
+	-- needs activation, cause sending to app in bg does not work
 	hs.application("Twitterrific"):activate()
-	hs.eventtap.keyStroke({"command"}, "k")
-	hs.eventtap.keyStroke({"command"}, "j")
-	hs.eventtap.keyStroke({"command"}, "1")
+	hs.eventtap.keyStroke({"cmd"}, "k") -- mark all as red
+	hs.eventtap.keyStroke({"cmd"}, "j") -- scroll up
+	hs.eventtap.keyStroke({"cmd"}, "1") -- scroll up failsafe
+	hs.eventtap.keyStroke({}, "down") -- enable j/k movement
 
-	hs.application(frontapp):activate()
+	hs.application(previousApp):activate()
 end
 
-hs.hotkey.bind({}, "", twitterrificScrollUp)
+twitterUpdateInterval = 10
+hs.timer.doEvery(twitterUpdateInterval * 60, twitterrificScrollUp)
+
+function twitterificAction (key)
+	hs.eventtap.keyStroke({}, key, 1, hs.application("Twitterrific"))
+end
+
+hs.hotkey.bind({}, "home", twitterrificScrollUp)
+hs.hotkey.bind({}, "pagedown", function() twitterificAction("j") end, nil, function() twitterificAction("j") end)
+hs.hotkey.bind({}, "pageup", function() twitterificAction("k") end, nil, function() twitterificAction("k") end)
