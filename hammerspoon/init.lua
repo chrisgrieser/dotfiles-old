04458777a8b4a9ac77dba09fb3a2b251f1fa39fe
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-26 00:00:04 +0200

    Sun, 00:00, 7 files, Chris-M1-iMac
     M "Cron Jobs/frequent.log"
     M "Sublime User Folder/LSP-lua.sublime-settings"
     M hammerspoon/init.lua
    ?? hammerspoon/Spoons/ColorPicker.spoon/docs.json
    ?? hammerspoon/Spoons/ColorPicker.spoon/init.lua
    ?? hammerspoon/Spoons/KSheet.spoon/docs.json
    ?? hammerspoon/Spoons/KSheet.spoon/init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,11 +5,13 @@
 hs.window.animationDuration = 0
 
 --------------------------------------------------------------------------------
+-- Meta
 require("hammerspoon-reloader")
 require("utils")
 require("console")
-require("scroll")
 
+-- Base
+require("scroll")
 require("watchers")
 require("system-states")
 require("window-management")
@@ -18,8 +20,15 @@ require("private")
 require("menubar")
 require("discord")
 require("twitterrific-iina")
+
+-- Spoons
+-- https://github.com/Hammerspoon/hammerspoon/blob/master/SPOONS.md
 require("vim-mode")
 
+hs.loadSpoon("ColorPicker")
+spoon.ColorPicker:start()
+spoon.ColorPicker:bindHotkeys({show = { hyper, "z" } })
+hs.loadSpoon("KSheet")
 
 --------------------------------------------------------------------------------
 notify("Config reloaded")
