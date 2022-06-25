25cdbcf099a0547f72f51c46e5f655fd33561870
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-25 19:45:04 +0200

    Sat, 19:45, 3 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
     M Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/info.plist
     M "Cron Jobs/frequent.log"

--- Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env zsh
-QUERY=$(echo "$*" | tr " " "+")
-CHEAT=$(curl -s "https://cht.sh/$QUERY?T")
+QUERY=$(echo "$*" | sed 's/ /\//' | tr " " "+") # first space → /, all other spaces "+" for url
+CHEAT=$(curl -s "https://cht.sh/$QUERY?T") # https://cht.sh/:help
 
 # copy to clipboard
 echo "$CHEAT" | pbcopy
 
-# highlighted preview window via peek
+# highlighted preview window via Peek
 LANG="sh"
 [[ "$QUERY" =~ "/" ]] && LANG=$(echo "$QUERY" | cut -d"/" -f1)
 # shellcheck disable=SC2154
-
 [[ ! -d "$alfred_workflow_cache" ]] && mkdir -p "$alfred_workflow_cache"
 CACHE="$alfred_workflow_cache/temp.$LANG"
 echo "$CHEAT" > "$CACHE"
