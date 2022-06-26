dc265e1afc3e830e33ac543cdbb539d4b5dc6b9e
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-26 16:00:04 +0200

    Sun, 16:00, 3 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
     M "Cron Jobs/frequent.log"
     M hammerspoon/scroll.lua

--- Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
@@ -5,15 +5,26 @@ CHEAT=$(curl -s "https://cht.sh/$QUERY?Tq") # https://cht.sh/:help
 # copy to clipboard
 echo "$CHEAT" | pbcopy
 
-# highlighted preview window via Peek
 LANG="sh"
 if [[ "$QUERY" =~ "/" ]] ; then
 	LANG=$(echo "$QUERY" | cut -d"/" -f1)
 	QUERY=$(echo "$QUERY" | cut -d"/" -f2- | tr "+" " ")
 fi
-# shellcheck disable=SC2154
-[[ ! -d "$alfred_workflow_cache" ]] && mkdir -p "$alfred_workflow_cache"
-CACHE="$alfred_workflow_cache/$QUERY.$LANG"
-echo "$CHEAT" > "$CACHE"
-killall "qlmanage" # remove existing quicklook previews
-qlmanage -p "$CACHE"
+
+# view in Terminal
+alacritty \
+	--option=window.decorations=full \
+	--title="$LANG: $QUERY" \
+	--command="echo $CHEAT | $PAGER"
+
+
+alacritty --command man fd
+
+#-------------------------------------------------------------------------------
+# # quicklook preview via Peek
+# # shellcheck disable=SC2154
+# [[ ! -d "$alfred_workflow_cache" ]] && mkdir -p "$alfred_workflow_cache"
+# CACHE="$alfred_workflow_cache/$QUERY.$LANG"
+# echo "$CHEAT" > "$CACHE"
+# killall "qlmanage" # remove existing quicklook previews
+# qlmanage -p "$CACHE"
