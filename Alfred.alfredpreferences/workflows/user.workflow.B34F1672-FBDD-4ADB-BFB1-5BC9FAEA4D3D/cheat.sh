462ccc3b82c83395e310490be454cfbb57cdea59
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-27 01:45:04 +0200

    Mon, 01:45, 6 files, Chris-M1-iMac
     M .config/alacritty/alacritty.theme-switch-backup.yml
     M .config/alacritty/alacritty.yml
     M .config/alacritty/preview-window.yml
     M .config/alacritty/themes/.selected_theme
     M Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
     M "Cron Jobs/frequent.log"

--- Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.B34F1672-FBDD-4ADB-BFB1-5BC9FAEA4D3D/cheat.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
 
 QUERY=$(echo "$*" | sed 's/ /\//' | tr " " "+") # first space → /, all other spaces "+" for url
-CHEAT_INFO=$(curl -s "https://cht.sh/$QUERY?T") # https://cht.sh/:help
+CHEAT_INFO=$(curl -s "https://cht.sh/$QUERY") # https://cht.sh/:help
 CHEAT_CODE_ONLY=$(curl -s "https://cht.sh/$QUERY?TQ")
 
 # remove signature
@@ -18,32 +18,24 @@ if [[ "$QUERY" =~ "/" ]] ; then
 fi
 
 CACHE=~"/Library/Caches/$QUERY.$LANG"
-echo "${LANG:u} -- $QUERY" > "$CACHE" # header bar
+echo "${LANG:u} — $QUERY" > "$CACHE" # header bar
 echo "" >> "$CACHE"
 echo "$CHEAT_INFO" >> "$CACHE"
 
 #-------------------------------------------------------------------------------
 # PREVIEW VIA BAT
 PREVIEW_CONFIG=~/.config/alacritty/preview-window.yml
-BG_COLOR=#303643
-STATUSLINE_COLOR=#5E6F8A
-BAT_SYNTAX_THEME="Nord" # `bat --list-themes` for more themes
 
 alacritty \
 	--config-file="$PREVIEW_CONFIG" \
-	--option="colors.primary.background='$BG_COLOR'" \
-	--option="colors.primary.foreground='$STATUSLINE_COLOR'" \
-	--title="Cheat Sheet"\
-	--command bat "$CACHE" \
-		--file-name="$LANG – $QUERY" \
-		--language="$LANG" \
-		--theme="$BAT_SYNTAX_THEME" \
-		--style=snip \
-		--highlight-line=1 \
-		--pager="less -R --long-prompt +Ggk --window=-4 --incsearch --ignore-case --HILITE-UNREAD --tilde"
-# the pager options needs to be set explicitly, as without
-# bat sets "quit-if-one-screen" as a less option, closing most cheatsheets
-# causing in turn alacritty to quit before showing anything
+	--command less -R \
+		--long-prompt +Ggk \
+		--window=-4 \
+		--incsearch \
+		--ignore-case \
+		--HILITE-UNREAD \
+		--tilde \
+		-- "$CACHE"
 
 #-------------------------------------------------------------------------------
 # quicklook preview via peek
