15c401dd920e6138e37144dc7c2e43a4ab8c3a1e
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-27 23:00:04 +0200

    Mon, 23:00, 6 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/hs-docs-search.js
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/info.plist
     D Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/search-hammerspoon-docs_[Browser].applescript
     D Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/search-hammerspoon-plugins_[Browser].applescript
     M "Cron Jobs/frequent.log"
     M espanso/match/alfred.yml

--- Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/hs-docs-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/hs-docs-search.js
@@ -6,21 +6,16 @@ const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
 
 const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/repos/Hammerspoon/hammerspoon.github.io/git/trees/master?recursive=1"'))
 	.tree
-	.filter(file => /^docs\/(?!\d)/.test(file.path))
+	.filter(file => file.startsWith("docs/hs"))
+	.map(item => item.path)
 	.map(item => {
-		// fill in here
+		const subsite = item.slice(5, -5); // remove "/docs" and ".html"
 		return {
-			"title": item,
-			"match": alfredMatcher (item),
-			"subtitle": item,
-			"type": "file:skipcheck",
-			"icon": {
-				"type": "fileicon",
-				"path": item
-			},
-			"arg": item,
-			"uid": item,
+			"title": subsite,
+			"match": alfredMatcher (subsite),
+			"arg": subsite,
+			"uid": subsite,
 		};
-});
+	});
 
 JSON.stringify({ items: workArray });
