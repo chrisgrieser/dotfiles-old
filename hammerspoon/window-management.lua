7ab59d9999f60ff99e829514920bd404f52754eb
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-27 23:45:04 +0200

    Mon, 23:45, 2 files, Chris-M1-iMac
     M "Cron Jobs/frequent.log"
     M hammerspoon/window-management.lua

--- hammerspoon/window-management.lua
+++ hammerspoon/window-management.lua
@@ -153,6 +153,7 @@ displayWatcher:start()
 -- Open windows always on the screen where the mouse is
 function alwaysOpenOnMouseDisplay(_, eventType, appObject)
 	local numberOfScreens = #(hs.screen.allScreens())
+	if numberOfScreens == 1 then return end
 	if (numberOfScreens > 1 and eventType == hs.application.watcher.launched) then
 		local mouseScreen = hs.mouse.getCurrentScreen()
 
@@ -163,9 +164,8 @@ function alwaysOpenOnMouseDisplay(_, eventType, appObject)
 
 			if (mouseScreen:name() == screenOfWindow:name()) then return end
 			appWin:moveToScreen(mouseScreen)
-
 		end):start()
-
+	elseif () then
 	end
 end
 launchWhileMultiScreenWatcher = hs.application.watcher.new(alwaysOpenOnMouseDisplay)
