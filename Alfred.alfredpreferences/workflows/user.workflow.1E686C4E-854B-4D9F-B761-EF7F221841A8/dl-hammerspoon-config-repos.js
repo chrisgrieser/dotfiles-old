c405b7fa5c393763557e1dc6fde367e0db64fdb4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-30 16:15:03 +0200

    Thu, 16:15, 3 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/dl-hammerspoon-config-repos.js
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/info.plist
     M "Cron Jobs/frequent.log"

--- Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/dl-hammerspoon-config-repos.js
+++ Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/dl-hammerspoon-config-repos.js
@@ -4,16 +4,15 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const onlineJSON = (url) => JSON.parse(app.doShellScript("curl -sL '" + url + "'"));
 
-const dlFolder = "!wd"
+const dlFolder = "/Users/chrisgrieser/Library/Mobile Documents/com~apple~CloudDocs/File Hub/";
+app.doShellScript(`mkdir '${dlFolder}'/hs-repos || true`);
 
-const listOfRepos = onlineJSON("https://api.github.com/search/repositories?q=topic:hammerspoon-configuration")
+let i = 0;
+onlineJSON("https://api.github.com/search/repositories?q=topic:hammerspoon-configuration")
 	.items
-	.slice(0, 3)
-	.map (item => item.html_url)
-	.forEach (repo => {
-
-		app.doShellScript('cdgit clone')
-	})
-
-
-JSON.stringify(listOfRepos);
+	.forEach (item => {
+		const name = item.full_name + i.toString();
+		i++;
+		app.doShellScript(`cd '${dlFolder}'/hs-repos ; git clone --depth=1 '${item.html_url}' ${name}`);
+		console.log(`done: ${name}`);
+	});
