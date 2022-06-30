e6441173c427154e37b00f819f43c8287435a1ae
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-30 17:15:03 +0200

    Thu, 17:15, 3 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/info.plist
     M "Cron Jobs/frequent.log"

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
@@ -1,21 +1,26 @@
 #!/usr/bin/env osascript -l JavaScript
-ObjC.import("stdlib");
-const app = Application.currentApplication();
-app.includeStandardAdditions = true;
-const onlineJSON = (url) => JSON.parse(app.doShellScript("curl -sL '" + url + "'"));
 
-const dlFolder = $.getenv("working_folder").replace(/^~/, app.pathTo("home folder"));
+function run(argv){
+	ObjC.import("stdlib");
+	const app = Application.currentApplication();
+	app.includeStandardAdditions = true;
+	const onlineJSON = (url) => JSON.parse(app.doShellScript("curl -sL '" + url + "'"));
+	const topic = argv.join("");
 
-try {
-	app.doShellScript(`mkdir '${dlFolder}/hs-repos'`);
-} catch {}
+	const dlFolder = $.getenv("working_folder").replace(/^~/, app.pathTo("home folder")) + "/" + topic;
 
-onlineJSON("https://api.github.com/search/repositories?q=topic:hammerspoon-configuration")
-	.items
-	.forEach (item => {
-		const name = item.full_name.replaceAll("/", "-");
-		app.doShellScript(`cd '${dlFolder}'/hs-repos ; git clone --depth=1 '${item.html_url}' ${name}`);
-		console.log(`done: ${item.full_name}`);
-	});
+	try {
+		app.doShellScript(`mkdir -p '${dlFolder}'`);
+	} catch {}
 
-app.doShellScript(`open '${dlFolder}/hs-repos'`);
+	console.log("https://api.github.com/search/repositories?q=topic:" + topic);
+	onlineJSON("https://api.github.com/search/repositories?q=topic:" + topic)
+		.items
+		.forEach (item => {
+			const name = item.full_name.replaceAll("/", "-");
+			app.doShellScript(`cd '${dlFolder}' ; git clone --depth=1 '${item.html_url}' ${name}`);
+			console.log(`done: ${item.full_name}`);
+		});
+
+	app.doShellScript(`open '${dlFolder}'`);
+}
