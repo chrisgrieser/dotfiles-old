aefc70206544d60769b5f0b70a1d4cf4bb59f173
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-30 17:30:03 +0200

    Thu, 17:30, 4 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/info.plist
     M "Cron Jobs/frequent.log"

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
@@ -5,6 +5,7 @@ function run(argv){
 	const app = Application.currentApplication();
 	app.includeStandardAdditions = true;
 	const onlineJSON = (url) => JSON.parse(app.doShellScript("curl -sL '" + url + "'"));
+	const maxReposNumber = Number($.getenv("maxReposNumber"));
 	const topic = argv.join("");
 
 	const dlFolder = $.getenv("working_folder").replace(/^~/, app.pathTo("home folder")) + "/" + topic;
@@ -13,9 +14,11 @@ function run(argv){
 		app.doShellScript(`mkdir -p '${dlFolder}'`);
 	} catch {}
 
-	console.log("https://api.github.com/search/repositories?q=topic:" + topic);
-	onlineJSON("https://api.github.com/search/repositories?q=topic:" + topic)
+	const reposJSON = onlineJSON("https://api.github.com/search/repositories?q=topic:" + topic);
+
+	reposJSON
 		.items
+		.slice(0, maxReposNumber)
 		.forEach (item => {
 			const name = item.full_name.replaceAll("/", "-");
 			app.doShellScript(`cd '${dlFolder}' ; git clone --depth=1 '${item.html_url}' ${name}`);
@@ -23,4 +26,8 @@ function run(argv){
 		});
 
 	app.doShellScript(`open '${dlFolder}'`);
+
+	const notificationMsg = reposJSON.total_count.toString();
+	if (reposJSON.incomplete_results) notificationMsg (" (incomplete)");
+	return notificationMsg;
 }
