de1b48ff8a6402479aed4d1793dfa9a6d5c81498
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-30 17:00:03 +0200

    Thu, 17:00, 5 files, Chris-M1-iMac
     D Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/dl-hammerspoon-config-repos.js
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/info.plist
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/info.plist
     M "Cron Jobs/frequent.log"
    ?? Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/github-topic-search.js
@@ -4,8 +4,11 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const onlineJSON = (url) => JSON.parse(app.doShellScript("curl -sL '" + url + "'"));
 
-const dlFolder = $.getenv("working_directory");
-app.doShellScript(`mkdir '${dlFolder}/hs-repos' || true`);
+const dlFolder = $.getenv("working_folder").replace(/^~/, app.pathTo("home folder"));
+
+try {
+	app.doShellScript(`mkdir '${dlFolder}/hs-repos'`);
+} catch {}
 
 onlineJSON("https://api.github.com/search/repositories?q=topic:hammerspoon-configuration")
 	.items
@@ -14,3 +17,5 @@ onlineJSON("https://api.github.com/search/repositories?q=topic:hammerspoon-confi
 		app.doShellScript(`cd '${dlFolder}'/hs-repos ; git clone --depth=1 '${item.html_url}' ${name}`);
 		console.log(`done: ${item.full_name}`);
 	});
+
+app.doShellScript(`open '${dlFolder}/hs-repos'`);
