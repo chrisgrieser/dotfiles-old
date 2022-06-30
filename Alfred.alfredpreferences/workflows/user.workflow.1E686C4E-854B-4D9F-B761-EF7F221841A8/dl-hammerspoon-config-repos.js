c2f3ab2774f885414afde4a416e0369bccaa9834
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-30 15:45:03 +0200

    Thu, 15:45, 3 files, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/dl-hammerspoon-config-repos.js
     M Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/info.plist
     M "Cron Jobs/frequent.log"

--- Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/dl-hammerspoon-config-repos.js
+++ Alfred.alfredpreferences/workflows/user.workflow.1E686C4E-854B-4D9F-B761-EF7F221841A8/dl-hammerspoon-config-repos.js
@@ -4,10 +4,20 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const onlineJSON = (url) => JSON.parse(app.doShellScript("curl -sL '" + url + "'"));
 
-const listofRepos = onlineJSON("https://api.github.com/search/repositories?q=topic:hammerspoon-configuration")
-	.items
-	.map(item => item.full_name)
-	.forEach(repo =>{
-		luaFiles = onlineJSON()
-	})
+const listOfLuaFiles = [];
+
+onlineJSON("https://api.github.com/search/repositories?q=topic:hammerspoon-configuration")
+	.items[1]
+	.forEach(repo => {
+		const repoFiles = onlineJSON("https://api.github.com/repos/" + repo.full_name + "/git/trees/" + repo.default_branch + "?recursive=1");
+		if (!repoFiles) return;
+		repoFiles
+			.tree
+			.map(item => item.path)
+			.filter(file => file.endsWith(".lua"))
+			.forEach(luaFile => listOfLuaFiles.push(repo + "/" + luaFile));
+		console.log("done one");
+	});
+
+JSON.stringify(listOfLuaFiles);
 
