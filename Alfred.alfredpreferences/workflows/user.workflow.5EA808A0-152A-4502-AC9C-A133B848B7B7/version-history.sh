4f6fe68a056d3e0833dc13b5f975b3b4487b2448
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-08 23:15:04 +0200

    Fri, 23:15, 1 file, Chris-M1-iMac
     M Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.sh

--- Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.sh
+++ Alfred.alfredpreferences/workflows/user.workflow.5EA808A0-152A-4502-AC9C-A133B848B7B7/version-history.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# get path of current Finder Selection
+# get path of current Finder Selection (macOS)
 FINDER_SEL=$(osascript -e 'tell application "Finder"
 	set sel to selection
 	if ((count sel) > 1) then return POSIX path of ((item 1 of sel) as text)
@@ -9,7 +9,7 @@ FINDER_SEL=$(osascript -e 'tell application "Finder"
 end tell')
 
 [[ "$FINDER_SEL" == "no selection" ]] && exit 1
-[[ -d "$FINDER_SEL" ]] && exit 1 # folder selected
+[[ -d "$FINDER_SEL" ]] && exit 1 # selection is not a file
 
 FOLDER=$(dirname "$FINDER_SEL")
 FILE=$(basename "$FINDER_SEL")
@@ -29,8 +29,8 @@ open "$output_dir"
 
 # https://stackoverflow.com/questions/1964142/how-can-i-list-all-the-different-versions-of-a-file-and-diff-them-also/32849134#32849134
 for commit_hash in $(git log --pretty=format:%h "$FILE") ; do
-	commit_date=$(git show -s --format=%ci "$commit_hash" | cut -c-16 | tr ":" "-")
-	out="$output_dir/$commit_date $commit_hash.$EXT"
+	commit_date=$(git show -s --format=%ci "$commit_hash" | cut -c-16)
+	out="$output_dir/$commit_date ($commit_hash).$EXT"
 	git show "$commit_hash:./$FILE" > "$out"
 done
 
