b426a1f1128211cd4b69068ab9f11b09bcf206ed
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-09 15:00:04 +0200

    Sat, 15:00, 1 file, Chris-M1-iMac
     M hammerspoon/discord.lua

--- hammerspoon/discord.lua
+++ hammerspoon/discord.lua
@@ -5,7 +5,7 @@ function discordWatcher(appName, eventType)
 
 	-- on launch, open OMG Server instead of friends (who needs friends if you have Obsidian?)
 	-- and reconnect Obsidian's Discord Rich Presence (Obsidian launch already covered by RP Plugin)
-	if appName ~= "Discord" and eventType == hs.application.watcher.launched then
+	if eventType == hs.application.watcher.launched then
 		hs.urlevent.openURL("discord://discord.com/channels/686053708261228577/700466324840775831")
 		if appIsRunning("Obsidian") then
 			runDelayed(2.5, function()
@@ -15,7 +15,7 @@ function discordWatcher(appName, eventType)
 		end
 
 	-- when Discord is focused, enclose URL in clipboard with <>
-	elseif appName ~= "Discord" and eventType == hs.application.watcher.activated then
+	elseif eventType == hs.application.watcher.activated then
 		local clipb = hs.pasteboard.getContents()
 		if not clipb then return end
 		local hasURL = string.match(clipb, '^https?%S+$')
@@ -24,7 +24,7 @@ function discordWatcher(appName, eventType)
 		end
 
 	-- when Discord is unfocused, removes <> from URL in clipboard
-	elseif appName ~= "Discord" and eventType == hs.application.watcher.deactivated then
+	elseif eventType == hs.application.watcher.deactivated then
 		local clipb = hs.pasteboard.getContents()
 		local hasEnclosedURL = string.match(clipb, '^<https?%S+>$')
 		if (hasEnclosedURL) then
